f937ad5a5318687a8f6218acf60ad798c7cdb0ec
DevalJambukiya <jambukiyadeval@gmail.com>
2024-10-24 18:13:03 +0530

    add web id and other

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,31 @@ import 'package:grocery_vegitable_market/screens/welcomescreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  if (kIsWeb) {
+    WidgetsFlutterBinding.ensureInitialized();
+    // Firebase initialization for web
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAag9sX1VGnL013PEpLuLil403KbmkqejI",
+        authDomain:
+            "grocery-vegetable-market-215f9.firebaseapp.com", // Firebase Auth domain for web
+        projectId: "grocery-vegetable-market-215f9",
+        storageBucket:
+            "grocery-vegetable-market-215f9.appspot.com", // Firebase Storage
+        messagingSenderId: "243939484600",
+        appId: "1:243939484600:android:27da5344c5ba7a9593254d",
+        measurementId:
+            "G-XXXXXXXXXX", // Optional: Use your Firebase Analytics measurement ID
+        databaseURL:
+            "https://grocery-vegetable-market-215f9.firebaseio.com", // Realtime Database URL (if needed)
+      ),
+    );
+    runApp(const MyApp());
+  } else {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
